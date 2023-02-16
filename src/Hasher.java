93d1dc8da00981c334f1314c38e42a791ab13bfc
Abuev Bakhtiiar <124647610+Bahandi73@users.noreply.github.com>
2023-02-16 18:31:43 +0600

    Update Hasher.java

--- src/Hasher.java
+++ src/Hasher.java
@@ -1,9 +1,22 @@
 package hasher;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 public class Hasher {
     public static String hashPassword(String text) {
         String hashPass = text.substring(0, 0);
-        hashPass = hashPass + "#########";
-        String temp = text.substring(text.length());
-        return hashPass + temp;
+        String hashedPassword = null;
+        try {
+            MessageDigest mdh = MessageDigest.getInstance("MD5");
+            mdh.update(hashPass.getBytes());
+            byte[] bytes = mdh.digest();
+            StringBuilder sbh = new StringBuilder();
+            for(int i = 0; i < bytes.length; i++){
+                sbh.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+            }
+            hashedPassword = sbh.toString();
+        }catch (NoSuchAlgorithmException e){
+            e.printStackTrace();
+        }
+        return hashedPassword;
     }
 }
